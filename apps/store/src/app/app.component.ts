a093fb0a378a4e7fb0f4093a3737e86b4d989fdf
dpresiana <dpresiana@vmware.com>
2023-04-10 14:25:53 +0300

    Lab 9
    Create the API util interface DTOs
    
    Move component (rename it)

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import { getAllGames } from '../fake-api';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 import { HttpClient } from '@angular/common/http';
+import { Game } from '@bg-hoard/util-interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   constructor(private http: HttpClient) {}
 
   title = 'Board Game Hoard';
-  games = this.http.get<any[]>('/api/games');
+  games: Observable<Game[]> = this.http.get<Game[]>('/api/games');
   formatRating = formatRating;
-
 }

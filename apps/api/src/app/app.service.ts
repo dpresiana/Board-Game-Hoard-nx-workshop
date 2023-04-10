1bf5327f6a6d67077eea2b87a3c2c3c04f3ecdff
dpresiana <dpresiana@vmware.com>
2023-04-10 17:10:08 +0300

    Lab 13
    Manual work to create util-lib
    Add notification lib and import it in the app service

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Game } from '@bg-hoard/util-interface';
+import { sendNotification } from '@bg-hoard/api/util-notifications'
 
 const games: Game[] = [
   {
@@ -32,5 +33,8 @@ const games: Game[] = [
 @Injectable()
 export class AppService {
   getAllGames = () => games;
-  getGame = (id: string) => games.find((game) => game.id === id);
+  getGame = (id: string) => games.find((game) => {
+    sendNotification(id);
+    return game.id === id
+  });
 }
